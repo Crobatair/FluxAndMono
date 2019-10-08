2e966e3de1ad5bdf21207946d08c6ca53e566ac2
Alejandro <VM13023@ues.edu.sv>
2019-10-07 21:11:53 -0600

    Initial commit of repository
    Initialized using  spring initializer

---
+++ src/main/java/com/crobatair/learn/fluxweb/FluxwebApplication.java
@@ -0,0 +1,13 @@
+package com.crobatair.learn.fluxweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crobatair/learn/fluxweb/FluxAndMonoTest.java
@@ -0,0 +1,4 @@
+package com.crobatair.learn.fluxweb;
+
+public class FluxAndMonoTest {
+}
